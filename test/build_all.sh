f6f40a0484ef7d42b4bbe3142838d288b2d8395d
David Hu <chin33z@gmail.com>
2015-11-20 16:34:36 +0800

    update SysLib build command

--- test/build_all.sh
+++ test/build_all.sh
@@ -5,68 +5,11 @@ bazel clean
 # --spawn_strategy=standalone --genrule_strategy=standalone : disable sandbox completely(docker
 # container may cause some permission deny)
 # -DQNAPNAS can be pass by --copt
+# --define block_size=4096
 
-bazel build --copt="-w" --config=x86_64 --linkopt=-Wl,--strip-all --dynamic_mode=fully --define block_size=4096 \
-@zlib-1.2.8//:z \
-@popt-1.14//:popt \
-@libid3tag-0.15.1b//:id3tag \
-@json-c-0.9//:json-c \
-@sqlite-3.4.1//:sqlite3 \
-@boost_1_53_0//:boost \
-@expat-2.0.1//:expat \
-@fio-2.2.10//:fio \
-@xmlrpc//:xmlrpc-epi \
-@libxml2-2.9.2//:xml2 \
-@eventlog-0.2.7//:evtlog \
-@rlog-1.4//:rlog \
-@jpeg-6b-src//:jpeg \
-@libevent-1.4.12-stable//:libevent \
-@pam-0.99.7.1//:linux-pam \
-@libffi-3.2.1//:ffi \
-@ncurses-5.5//:nc \
-@readline-5.2//:rl \
-@backtrace//:backtrace_tools \
-@gdbm-1.8.3//:libgdbm \
-@freetype-2.3.7//:freetype \
-@libtool-1.5.24//:ltdl \
-@libupnp-1.6.19//:libupnp \
-@libnfsidmap-0.23//:libnfsidmap \
-@libgssglue-0.4//:gssglue \
-@e2fsprogs-1.42.5//:com_err \
-@openssl-1.0.1p//:openssl-dev \
-@openldap-2.4.33//:openldap \
-@liboop-1.0//:oop \
-@ruli-0.33//:ruli \
-@libusb-1.0.9//:usb-1.0 \
-@libusb-compat-0.1.4//:libusb-compat \
-@postgresql-9.4.4//:pq \
-@cyrus-sasl-2.1.22//:sasl2 \
-@libgpg-error-1.6//:gpg-error \
-@libgcrypt-1.4.3//:gcrypt \
-@gnutls-2.6.2//:libgnutls \
-@libtasn1-1.6//:tasn1 \
-@bzip2-1.0.6//:bz2 \
-@fuse-2.9.2//:libfuse \
-@libaio-0.3.109//:aio \
-@ntfs-3g-1.2412//:ntfs-3g-files \
-@jrtplib-3.3.0//:jrtp \
-@liblinuxfallocate-glibc-2.17//:linuxfallocate \
-@libeventfd-glibc-2.17//:eventfd \
-@libutimensat-glibc-2.17//:utimensat \
-@thin-provisioning-tools-v0.4.1//:thintools \
-@tiff-4.0.4//:tiff \
-@ImageMagick-6.8.9-9//:IM \
-@attr-2.4.47//:libattr \
-@acl-2.2.49//:libacl \
-@cups-1.6.4//:libcups \
-@gsoap_2.7.17//:gsoap \
-@EtroSDK-src-2.3.3//:EtroSDK \
-@libcsv-3.0.2//:csv \
-@libcsv-3.0.2//:csv \
-@librsync-0.9.7//:rsync \
+bazel build --copt="-w" --config=x86_64 --linkopt=-Wl,--strip-all  //repo:SysLib
 
 
-# @gnupg-1.4.19//:gpg \
 
 # @libstdcxx-v3//:stdc++ \
 
