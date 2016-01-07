62f5d1d65c5ac4c14ccfceb3c25c1544d4b6c402
David Hu <chin33z@gmail.com>
2016-01-07 09:21:05 +0000

    fix

--- test/build_all.sh
+++ test/build_all.sh
@@ -19,7 +19,7 @@ bazel clean
 # --experimental_skip_static_outputs : will cause some problem
 # --define block_size=4096
 
-bazel build --copt="-w" --define block_size=4096 --config=x86_64 --linkopt=-Wl,--strip-all //repo:final
+bazel build --copt="-w" --define block_size=4096 --config=x86_64 --linkopt=-Wl,--strip-all //repo:SysLib
 
 
 
